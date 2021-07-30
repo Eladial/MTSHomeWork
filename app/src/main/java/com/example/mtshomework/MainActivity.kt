c2837512975f1a5cb33aba569ef398991dd1dec9
Магомед Амирханов <mvamirkhanov@miem.hse.ru>
2021-07-30 14:42:01 +0300

    Corrected something 2.0

--- app/src/main/java/com/example/mtshomework/MainActivity.kt
+++ app/src/main/java/com/example/mtshomework/MainActivity.kt
@@ -2,13 +2,12 @@ package com.example.mtshomework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.recyclerview.widget.RecyclerView
 import com.example.mtshomework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var fragment: MovieDetailsFragment? = null
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,23 +15,23 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigation = binding.bottomNavigation
         if (savedInstanceState == null)
-            supportFragmentManager.beginTransaction().replace(R.id.container_view, MovieDetailsFragment(), "FRAGMENT").commit()
+            supportFragmentManager.beginTransaction().replace(R.id.container_view, MovieDetailsFragment(), TAG_FRAGMENT).commit()
         else
-            fragment = supportFragmentManager.findFragmentByTag("FRAGMENT") as? MovieDetailsFragment
+            fragment = supportFragmentManager.findFragmentByTag(TAG_FRAGMENT) as? MovieDetailsFragment
         bottomNavigation.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.home -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.container_view, MovieDetailsFragment(), "FRAGMENT").commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.container_view, MovieDetailsFragment(), TAG_FRAGMENT).commit()
                     return@setOnItemSelectedListener true
                 }
                 R.id.profile -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.container_view, ProfileFragment(), "FRAGMENT").commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.container_view, ProfileFragment(), TAG_FRAGMENT).commit()
                     return@setOnItemSelectedListener true
                 }
                 else -> false
             }
         }
     }
-
-
 }
+
+private const val TAG_FRAGMENT = "FRAGMENT"
--- app/src/main/java/com/example/mtshomework/MovieFragment.kt
+++ app/src/main/java/com/example/mtshomework/MovieFragment.kt
@@ -1,7 +1,6 @@
 package com.example.mtshomework
 
 import android.annotation.SuppressLint
-import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
